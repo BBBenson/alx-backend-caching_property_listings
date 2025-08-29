9dca9a7bb1f51c06740a45445e04e87e5c1f8748
BBBenson <bbbenson26@gmail.com>
2025-08-29 09:51:27 +0000

    update utils

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,27 @@
 import logging
+from django.core.cache import cache
+from .models import Property
 from django_redis import get_redis_connection
+
+# properties/utils.py
+CACHE_KEY = "all_properties"
+CACHE_TIMEOUT = 3600  # 1 hour (in seconds)
+
+def get_all_properties():
+    """
+    Retrieves all Property objects, with Redis caching for 1 hour.
+    """
+    properties = cache.get(CACHE_KEY)
+
+    if properties is None:
+        print("Cache miss: Fetching from DB...")
+        properties = list(Property.objects.all())  # Convert queryset to list for caching
+        cache.set(CACHE_KEY, properties, CACHE_TIMEOUT)
+    else:
+        print("Cache hit: Loaded from Redis.")
+
+    return properties
+
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +35,4 @@ def get_redis_cache_metrics():
     metrics = {'hits': hits, 'misses': misses, 'hit_ratio': ratio}
     logger.info(f"Cache Metrics: {metrics}")
     return metrics
+
